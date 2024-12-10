6e166e8b4c2133afa52120afec169251d21b4937
bluntwise <alandely11@gmail.com>
2024-12-10 09:05:16 +0100

    Adress created

---
+++ Adress.java
@@ -0,0 +1,35 @@
+public class Adress {
+
+    private String city;
+    private int city_code;
+    private String street;
+
+    public Adress(String city, int city_code, String street){
+        this.city = city;
+        this.city_code = city_code;
+        this.street = street;
+    }
+
+    public String getCity(){
+        return this.city;
+    }
+
+    public int city_code(){
+        return this.city_code;
+    }
+
+    public String getStreet(){
+        return this.street;
+    }
+
+    public String toString(){
+        return this.street + " " + this.city + " " + Integer.toString(this.city_code);
+    }
+
+
+    public static void main(String[] args) {
+        
+        Adress a = new Adress("Lannion", 22300, "46 Avenue du Général de Gaulle");
+        System.out.println(a);
+    }
+}
